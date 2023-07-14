1b9b20bee3af0e01c9ae537c157f4d9d817958c9
mcworkaholic <94456069+mcworkaholic@users.noreply.github.com>
2023-07-14 10:06:35 -0500

    Update data_match.py
    Changed if, elif branches into it's their own function. Added additional command-line arguments.

--- Research Data/Python/data_match.py
+++ Research Data/Python/data_match.py
@@ -1,32 +1,31 @@
 import sys
 import pandas as pd
 
-def match_and_write_data(source_file, destination_file, sheet_name):
+def match_data(source_df, destination_df, column_names):
+    # Convert the 'Date' column in the destination file to datetime64[ns] type
+    destination_df['Date'] = pd.to_datetime(destination_df['Date'])
+
+    # Convert the 'Date' column in the source file to datetime64[ns] type
+    source_df['Date'] = pd.to_datetime(source_df['Date'])
+
+    # Iterate over each column name and match the dates and values
+    for column_name in column_names:
+        if column_name in source_df.columns and column_name in destination_df.columns:
+            matched_data = source_df[['Date', column_name]].dropna()
+
+            # Merge the matched data with the destination file
+            destination_df = pd.merge(destination_df, matched_data, on='Date', how='left')
+            destination_df = destination_df.rename(columns={column_name: column_name + '_source'})
+
+    return destination_df
+
+def match_and_write_data(source_file, destination_file, sheet_name, column_names):
     # Read the source and destination Excel files
     source_df = pd.read_excel(source_file)
     destination_df = pd.read_excel(destination_file, sheet_name=sheet_name, engine='openpyxl')
 
-    # Convert the 'Date' column in the destination file to datetime64[ns] type
-    destination_df['Date'] = pd.to_datetime(destination_df['Date'])
-
-    # Check if the 'DJIA' column exists in both source and destination files
-    if 'DJIA' in source_df.columns and 'DJIA' in destination_df.columns:
-        # Match the dates and DJIA values based on the 'Date' column
-        matched_data = source_df[['Date', 'DJIA']].dropna()
-
-        # Merge the matched data with the destination file
-        destination_df = pd.merge(destination_df, matched_data, on='Date', how='left')
-        destination_df = destination_df.rename(columns={'DJIA': 'DJIA_source'})
-    elif 'FTSE100' in source_df.columns and 'FTSE100' in destination_df.columns:
-        # Convert the 'Date' column in the source file to datetime64[ns] type
-        source_df['Date'] = pd.to_datetime(source_df['Date'])
-
-        # Match the dates and FTSE100 values based on the 'Date' column
-        matched_data = source_df[['Date', 'FTSE100']].dropna()
-
-        # Merge the matched data with the destination file
-        destination_df = pd.merge(destination_df, matched_data, on='Date', how='left')
-        destination_df = destination_df.rename(columns={'FTSE100': 'FTSE_source'})
+    # Call the match_data function with the column names list
+    destination_df = match_data(source_df, destination_df, column_names)
 
     # Save the updated destination file
     with pd.ExcelWriter(destination_file, engine='openpyxl', mode='a') as writer:
@@ -34,14 +33,19 @@ def match_and_write_data(source_file, destination_file, sheet_name):
 
 if __name__ == '__main__':
     # Check if the required command-line arguments are provided
-    if len(sys.argv) < 4:
-        print("Usage: python data_match.py '<source_file_path>' '<destination_file_path>' '<sheet_name>'")
+    if len(sys.argv) < 5:
+        # python data_match.py "source_file.xlsx" "destination_file.xlsx" "Sheet1" "DJIA,FTSE100,S&P500"
+        print("Usage: python data_match.py '<source_file_path>' '<destination_file_path>' '<sheet_name>' '<column_name1,column_name2,...>'")
         sys.exit(1)
 
-    # Extract the source and destination file paths and the sheet name from command-line arguments
+    # Extract the source file path, destination file path, sheet name, and column names from command-line arguments
     source_file_path = sys.argv[1]
     destination_file_path = sys.argv[2]
     sheet_name = sys.argv[3]
+    column_names = sys.argv[4].split(',')
 
     # Call the match_and_write_data function
-    match_and_write_data(source_file_path, destination_file_path, sheet_name)
+    match_and_write_data(source_file_path, destination_file_path, sheet_name, column_names)
+
+
+   
